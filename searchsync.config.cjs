e05e45ff200c8fa3083c6d819f2fc1337f63f2df
training5 <natthawat@harmonyx.co>
2024-02-21 02:29:20 +0700

    Update searchsync.config.cjs with new host URL

--- searchsync.config.cjs
+++ searchsync.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   server: {
     type: "meilisearch",
-    host: "http://host.docker.internal:7700",
+    host: "http://localhost:7700",
+    // _host: "http://host.docker.internal:7700",
     key: "a5ab9786f9310d07bf1b2cc16080bc53c0f893b98372b383ee347c266a199157",
   },
   reindexOnStart: true,
